7a545388958597fa90dc3fd537d4d2bdf763be31
Mu'aaz Bassa <muaazbassa@gmail.com>
2022-10-04 22:18:06 +0200

    Update StudentCourses.Designer.cs

--- StudentCourses.Designer.cs
+++ StudentCourses.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.pnl2 = new System.Windows.Forms.Panel();
+            this.lblCourseName1 = new System.Windows.Forms.Label();
+            this.lblCourseName2 = new System.Windows.Forms.Label();
+            this.lblAcademic1 = new System.Windows.Forms.Label();
+            this.lblAcademic2 = new System.Windows.Forms.Label();
+            this.pnl1.SuspendLayout();
+            this.pnl2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -36,23 +44,100 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(467, 308);
+            this.label1.Location = new System.Drawing.Point(572, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 28);
+            this.label1.Size = new System.Drawing.Size(136, 34);
             this.label1.TabIndex = 1;
             this.label1.Text = "Courses";
             // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.Color.Teal;
+            this.pnl1.Controls.Add(this.lblAcademic1);
+            this.pnl1.Controls.Add(this.lblCourseName1);
+            this.pnl1.ForeColor = System.Drawing.Color.White;
+            this.pnl1.Location = new System.Drawing.Point(130, 236);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(513, 340);
+            this.pnl1.TabIndex = 2;
+            // 
+            // pnl2
+            // 
+            this.pnl2.BackColor = System.Drawing.Color.Teal;
+            this.pnl2.Controls.Add(this.lblAcademic2);
+            this.pnl2.Controls.Add(this.lblCourseName2);
+            this.pnl2.Location = new System.Drawing.Point(741, 236);
+            this.pnl2.Name = "pnl2";
+            this.pnl2.Size = new System.Drawing.Size(513, 340);
+            this.pnl2.TabIndex = 3;
+            // 
+            // lblCourseName1
+            // 
+            this.lblCourseName1.AutoSize = true;
+            this.lblCourseName1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCourseName1.ForeColor = System.Drawing.Color.White;
+            this.lblCourseName1.Location = new System.Drawing.Point(186, 107);
+            this.lblCourseName1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCourseName1.Name = "lblCourseName1";
+            this.lblCourseName1.Size = new System.Drawing.Size(146, 34);
+            this.lblCourseName1.TabIndex = 2;
+            this.lblCourseName1.Text = "Course 1";
+            // 
+            // lblCourseName2
+            // 
+            this.lblCourseName2.AutoSize = true;
+            this.lblCourseName2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCourseName2.ForeColor = System.Drawing.Color.White;
+            this.lblCourseName2.Location = new System.Drawing.Point(186, 107);
+            this.lblCourseName2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCourseName2.Name = "lblCourseName2";
+            this.lblCourseName2.Size = new System.Drawing.Size(146, 34);
+            this.lblCourseName2.TabIndex = 3;
+            this.lblCourseName2.Text = "Course 2";
+            // 
+            // lblAcademic1
+            // 
+            this.lblAcademic1.AutoSize = true;
+            this.lblAcademic1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAcademic1.ForeColor = System.Drawing.Color.White;
+            this.lblAcademic1.Location = new System.Drawing.Point(186, 196);
+            this.lblAcademic1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAcademic1.Name = "lblAcademic1";
+            this.lblAcademic1.Size = new System.Drawing.Size(161, 34);
+            this.lblAcademic1.TabIndex = 3;
+            this.lblAcademic1.Text = "Ms. Neetu";
+            // 
+            // lblAcademic2
+            // 
+            this.lblAcademic2.AutoSize = true;
+            this.lblAcademic2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAcademic2.ForeColor = System.Drawing.Color.White;
+            this.lblAcademic2.Location = new System.Drawing.Point(186, 196);
+            this.lblAcademic2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAcademic2.Name = "lblAcademic2";
+            this.lblAcademic2.Size = new System.Drawing.Size(160, 34);
+            this.lblAcademic2.TabIndex = 4;
+            this.lblAcademic2.Text = "Ms. Spark";
+            // 
             // StudentCourses
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(25)))), ((int)(((byte)(56)))));
-            this.ClientSize = new System.Drawing.Size(1080, 676);
+            this.ClientSize = new System.Drawing.Size(1440, 832);
+            this.Controls.Add(this.pnl2);
+            this.Controls.Add(this.pnl1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "StudentCourses";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "StudentCourses";
+            this.pnl1.ResumeLayout(false);
+            this.pnl1.PerformLayout();
+            this.pnl2.ResumeLayout(false);
+            this.pnl2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -61,5 +146,11 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Panel pnl2;
+        private System.Windows.Forms.Label lblAcademic1;
+        private System.Windows.Forms.Label lblCourseName1;
+        private System.Windows.Forms.Label lblAcademic2;
+        private System.Windows.Forms.Label lblCourseName2;
     }
 }
