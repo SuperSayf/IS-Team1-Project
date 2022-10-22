ccef65b34ee7a9030f05034366d7fec8834c75d6
supersayf <sayfullah.jumoorty@live.com>
2022-10-22 23:58:03 +0200

    Student side quiz and query done

--- StudentQuizManager.Designer.cs
+++ StudentQuizManager.Designer.cs
@@ -33,7 +33,12 @@
             this.btnMedium = new System.Windows.Forms.Button();
             this.btnHard = new System.Windows.Forms.Button();
             this.lblCourseName = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flpQueries = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.btnSendQuery = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblText
@@ -42,7 +47,7 @@
             this.lblText.Cursor = System.Windows.Forms.Cursors.Default;
             this.lblText.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblText.ForeColor = System.Drawing.Color.White;
-            this.lblText.Location = new System.Drawing.Point(143, 175);
+            this.lblText.Location = new System.Drawing.Point(133, 175);
             this.lblText.Name = "lblText";
             this.lblText.Size = new System.Drawing.Size(195, 18);
             this.lblText.TabIndex = 14;
@@ -50,7 +55,11 @@
             // 
             // btnEasy
             // 
-            this.btnEasy.Location = new System.Drawing.Point(181, 224);
+            this.btnEasy.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnEasy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEasy.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F);
+            this.btnEasy.ForeColor = System.Drawing.Color.White;
+            this.btnEasy.Location = new System.Drawing.Point(171, 224);
             this.btnEasy.Name = "btnEasy";
             this.btnEasy.Size = new System.Drawing.Size(124, 38);
             this.btnEasy.TabIndex = 15;
@@ -60,7 +69,12 @@
             // 
             // btnMedium
             // 
-            this.btnMedium.Location = new System.Drawing.Point(181, 295);
+            this.btnMedium.Enabled = false;
+            this.btnMedium.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnMedium.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMedium.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F);
+            this.btnMedium.ForeColor = System.Drawing.Color.White;
+            this.btnMedium.Location = new System.Drawing.Point(171, 295);
             this.btnMedium.Name = "btnMedium";
             this.btnMedium.Size = new System.Drawing.Size(124, 38);
             this.btnMedium.TabIndex = 16;
@@ -70,7 +84,12 @@
             // 
             // btnHard
             // 
-            this.btnHard.Location = new System.Drawing.Point(181, 366);
+            this.btnHard.Enabled = false;
+            this.btnHard.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnHard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHard.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F);
+            this.btnHard.ForeColor = System.Drawing.Color.White;
+            this.btnHard.Location = new System.Drawing.Point(171, 366);
             this.btnHard.Name = "btnHard";
             this.btnHard.Size = new System.Drawing.Size(124, 38);
             this.btnHard.TabIndex = 17;
@@ -84,31 +103,78 @@
             this.lblCourseName.Cursor = System.Windows.Forms.Cursors.Default;
             this.lblCourseName.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCourseName.ForeColor = System.Drawing.Color.White;
-            this.lblCourseName.Location = new System.Drawing.Point(416, 36);
+            this.lblCourseName.Location = new System.Drawing.Point(392, 32);
             this.lblCourseName.Name = "lblCourseName";
             this.lblCourseName.Size = new System.Drawing.Size(202, 24);
             this.lblCourseName.TabIndex = 18;
             this.lblCourseName.Text = "Course Name Here";
             this.lblCourseName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblCourseName.Click += new System.EventHandler(this.lblCourseName_Click);
             // 
-            // tableLayoutPanel1
+            // flpQueries
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(775, 246);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 9;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 116);
-            this.tableLayoutPanel1.TabIndex = 19;
+            this.flpQueries.AutoScroll = true;
+            this.flpQueries.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            this.flpQueries.Location = new System.Drawing.Point(598, 175);
+            this.flpQueries.Name = "flpQueries";
+            this.flpQueries.Size = new System.Drawing.Size(315, 182);
+            this.flpQueries.TabIndex = 19;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(592, 457);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(315, 1);
+            this.panel1.TabIndex = 21;
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(25)))), ((int)(((byte)(56)))));
+            this.txtQuery.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtQuery.ForeColor = System.Drawing.Color.White;
+            this.txtQuery.Location = new System.Drawing.Point(592, 440);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(315, 13);
+            this.txtQuery.TabIndex = 20;
+            // 
+            // btnSendQuery
+            // 
+            this.btnSendQuery.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnSendQuery.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSendQuery.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendQuery.ForeColor = System.Drawing.Color.White;
+            this.btnSendQuery.Location = new System.Drawing.Point(699, 478);
+            this.btnSendQuery.Name = "btnSendQuery";
+            this.btnSendQuery.Size = new System.Drawing.Size(110, 25);
+            this.btnSendQuery.TabIndex = 22;
+            this.btnSendQuery.Text = "Send";
+            this.btnSendQuery.UseVisualStyleBackColor = true;
+            this.btnSendQuery.Click += new System.EventHandler(this.btnSendQuery_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(682, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 18);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Query responses";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(696, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 18);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Send a query";
             // 
             // StudentQuizManager
             // 
@@ -116,7 +182,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(25)))), ((int)(((byte)(56)))));
             this.ClientSize = new System.Drawing.Size(1080, 676);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSendQuery);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.flpQueries);
             this.Controls.Add(this.lblCourseName);
             this.Controls.Add(this.btnHard);
             this.Controls.Add(this.btnMedium);
@@ -139,6 +210,11 @@
         private System.Windows.Forms.Button btnMedium;
         private System.Windows.Forms.Button btnHard;
         private System.Windows.Forms.Label lblCourseName;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flpQueries;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button btnSendQuery;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- StudentQuizManager.cs
+++ StudentQuizManager.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace IS_Team1_Project
@@ -23,6 +18,71 @@ namespace IS_Team1_Project
         {
             // Set the course header
             lblCourseName.Text = StudentCourses.selectedCourse;
+
+            // Fetch database info
+            string connectionstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + frmLogin.dbPath;
+
+            using (OleDbConnection connection = new OleDbConnection(connectionstring))
+            {
+                connection.Open();
+                // SQL to find where in the queries table the query_answered is true and query_course is selectedCourse
+                OleDbCommand command = new OleDbCommand("SELECT * FROM queries WHERE query_answered = @query_answered AND query_course = @query_course", connection);
+                command.Parameters.AddWithValue("@query_answered", true);
+                command.Parameters.AddWithValue("@query_course", StudentCourses.selectedCourse);
+                OleDbDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // Dynamically adds the query_answer as a panel to the FlowLayout panel
+                    Panel panel = new Panel();
+                    panel.Size = new Size(315, 150);
+                    panel.BackColor = Color.FromArgb(10, 20, 56);
+                    panel.BorderStyle = BorderStyle.FixedSingle;
+                    panel.Margin = new Padding(0, 0, 0, 0);
+                    panel.Padding = new Padding(0, 0, 0, 0);
+                    panel.Name = reader["query_answer"].ToString();
+
+                    Label lblQuery = new Label();
+                    lblQuery.Text = "Q: " + reader["query_question"].ToString() + "\n\n" + "A: " + reader["query_answer"].ToString();
+                    lblQuery.ForeColor = Color.White;
+                    lblQuery.Font = new Font("Microsoft Sans Serif", 12);
+                    lblQuery.Location = new Point(10, 10);
+                    lblQuery.AutoSize = true;
+                    lblQuery.MaximumSize = new Size(300, 0);
+                    lblQuery.Name = "lblQuery";
+                    panel.Controls.Add(lblQuery);
+
+                    // Add the panel to the flowlayout panel
+                    flpQueries.Controls.Add(panel);
+                }
+
+                connection.Close();
+            }
+
+            using (OleDbConnection connection = new OleDbConnection(connectionstring))
+            {
+                connection.Open();
+                // SQL to find where in the quiz table quiz_pass is true and quiz_course is selectedCourse
+                OleDbCommand command = new OleDbCommand("SELECT * FROM quiz WHERE quiz_pass = @quiz_pass AND quiz_course = @quiz_course", connection);
+                command.Parameters.AddWithValue("@quiz_pass", true);
+                command.Parameters.AddWithValue("@quiz_course", StudentCourses.selectedCourse);
+                OleDbDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if (reader["quiz_difficulty"].ToString() == "Medium")
+                    {
+                        btnMedium.Enabled = true;
+                    }
+
+                    if (reader["quiz_difficulty"].ToString() == "Hard")
+                    {
+                        btnHard.Enabled = true;
+                    }
+                }
+
+                connection.Close();
+            }
         }
 
         private void btnEasy_Click(object sender, EventArgs e)
@@ -41,6 +101,36 @@ namespace IS_Team1_Project
         {
             quizDifficulty = "Hard";
             StudentMain.loadform(new StudentQuizView());
+        }
+
+        private void btnSendQuery_Click(object sender, EventArgs e)
+        {
+            // Validate if txtQuery is not empty
+            if (txtQuery.Text != "")
+            {
+                // Add to database
+                string connectionstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + frmLogin.dbPath;
+
+                using (OleDbConnection connection = new OleDbConnection(connectionstring))
+                {
+                    connection.Open();
+                    // SQL to insert into queries student_num and query_question
+                    OleDbCommand command = new OleDbCommand("INSERT INTO queries (student_num, query_question, query_course) VALUES (@student_num, @query_question, @query_course)", connection);
+                    command.Parameters.AddWithValue("@student_num", frmLogin.StudentNum);
+                    command.Parameters.AddWithValue("@query_question", txtQuery.Text);
+                    command.Parameters.AddWithValue("@query_course", StudentCourses.selectedCourse);
+                    command.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+
+                // Clear the txtQuery
+                txtQuery.Text = "";
+            }
+        }
+
+        private void lblCourseName_Click(object sender, EventArgs e)
+        {
         }
     }
 }
